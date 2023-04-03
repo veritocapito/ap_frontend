71905daa000f55840b87b36ffdb4c6c4f9d81c61
Veronica Capobianco <veritocapito@gmail.com>
2023-04-02 23:49:29 -0300

    working on login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'portfolio', component:DashboardComponent},
+  {path: 'login', component:LoginComponent},
+  {path: '**', redirectTo: '/portfolio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm=this.formBuilder.group({
+    email: ['veritocapito@gmail.com', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  })
+  constructor (private formBuilder:FormBuilder) {}
 
+  ngOnInit():void{}
+
+    //metodos para el formulario
+
+    get Password(){
+      return this.loginForm.get("password");
+    }
+  
+    get Mail(){
+      return this.loginForm.get("email");
+    }
+  
+    get PasswordValid(){
+      return this.Password?.touched && !this.Password?.valid;
+    }
+    
+    get MailValid(){
+      return this.Mail?.touched && !this.Mail?.valid;
+    }
+    
+  
+    onSubmit(event: Event){
+      //detenemos la ejecucion del comportamiento del submit del form
+      event.preventDefault;
+  
+      if (this.loginForm.valid){
+        //Llamamos a nuestro servicio para enviar los datos al servidor. 
+        //Tambien podriamos ejecutar alguna logica extra.
+        console.log("Todo ok! Formulario Enviado!");
+      } else {
+        //Corremos todas las validaciones para que se ejecuten los mensajes de error en el template.
+        this.loginForm.markAllAsTouched();
+      }
+  
+    }
 }
